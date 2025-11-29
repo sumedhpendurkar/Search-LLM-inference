c6aa1da8b930d13a2fba544b9ea9cb4fd7f912e2
Sumedh Pendurkar <22286878+sumedhpendurkar@users.noreply.github.com>
2025-11-29 10:14:39 +0000

    for paper

--- examples/ToT/prontoqa/dfs.sh
+++ examples/ToT/prontoqa/dfs.sh
@@ -2,6 +2,7 @@ CUDA_VISIBLE_DEVICES=0
 export llama_path="/home/sumedh/meta-llama/" # or "your/path/to/llama2"
 export llama_size="1B" # or "7B" of llama2
 export base_lm="$1" # or "llama2"
+#export model_dir="Qwen/Qwen2-7B-Instruct"
+export model_dir="meta-llama/Llama-3.2-3B"
 
-
-python -m torch.distributed.run --nproc_per_node 1 examples/ToT/prontoqa/tot_inference.py --depth_limit 10 --base_lm $base_lm --model_dir $llama_path --temperature 0.8 --search_algo dfs --total_states 10 --max_per_state 3 --llama_size $llama_size --log_dir logs/dfs_prontoqa
+python -m torch.distributed.run --nproc_per_node 1 examples/ToT/prontoqa/tot_inference.py --depth_limit 10 --base_lm hf --model_dir $model_dir --temperature 0.8 --search_algo dfs --total_calls 100 --max_per_state 3 --llama_size $llama_size --log_dir logs/dfs_prontoqa
